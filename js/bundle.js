153afc27566849ade841b0fd69b1c51155521283
Derya Antonelli <derya.antonelli@gmail.com>
2020-06-24 14:07:34 +0100

    Set click audio effect

---
+++ js/bundle.js
@@ -0,0 +1,6 @@
+//click sound
+const clickSound = document.querySelector('.click-sound');
+document.querySelector('body').addEventListener('click', function () {
+    clickSound.autoplay = true;
+    clickSound.load();
+  })
