8444bcd654f2668ebba4254c6288cbb6a1c686f2
Derya Antonelli <derya.antonelli@gmail.com>
2020-09-09 12:43:14 +0100

    Modify existing function to manage z-index of active windows, so that user can reclick an active window and see it coming on top of other windows

--- js/app.js
+++ js/app.js
@@ -16,27 +16,33 @@ desktop.buttons.closeButton = document.querySelectorAll(".close-btn");
 desktop.files = document.querySelectorAll(".file");
 
 
-//BUG -> ONCE CLICKED AGAIN, ALREADY OPEN WINDOW SHOULD REMOVED TO THE LAST INDEX
-desktop.handleClickIcon = function (event) {
-  var ele = desktop.getElement(event.currentTarget.href);
-  ele.classList.add("clicked");
+desktop.openWindow = function (event) {
+  var window = desktop.getDomElementFromUrl(event.currentTarget.href);
+  window.classList.add("clicked");
  
-  if(desktop.activeWindows.indexOf(ele) === -1) {
-    desktop.activeWindows.push(ele);
-    ele.style.zIndex = `${desktop.activeWindows.indexOf(ele)+1}`;
-  } else {
-    //if already on open windows, find item on open windows list and pop out
-    var index = desktop.activeWindows.indexOf(ele);
-    desktop.activeWindows.splice(index, 1);
-    desktop.activeWindows.push(ele);
-    //console.log("index is "+desktop.activeWindows.indexOf(ele))
-    ele.style.zIndex = `${desktop.activeWindows.indexOf(ele)}`;
-  } 
-  console.log(desktop.activeWindows)
+  var navigate = function(ele, arr) {
+    //find if open window is clicked before
+    if(arr.indexOf(ele) === -1) {
+      arr.push(ele);
+      for(let i=0; i<arr.length; i++) {
+        arr[i].style.zIndex = `${i+1}`;
+      }
+    } else {
+      //if window is already clicked, bring it on the top
+      var index = arr.indexOf(ele);
+      arr.splice(index, 1);
+      arr.push(ele);
+      for(let i=0; i<arr.length; i++) {
+        arr[i].style.zIndex = `${i+1}`;
+      }
+    } 
+  }
+  navigate(window, desktop.activeWindows);
 };
 
+
 //return corresponding element from url location
-desktop.getElement = function (urlLink) {
+desktop.getDomElementFromUrl = function (urlLink) {
   if (urlLink !== undefined && urlLink.includes("#")) {
     var regex = /#/;
     var hashIndexInLink = urlLink.search(regex);
@@ -45,46 +51,37 @@ desktop.getElement = function (urlLink) {
       var ele = document.querySelector(`#${elementIdAfterHash}`);
     }
   }
-
   return ele;
 };
 
-
-//change z-index of panes
-/*desktop.locationHashChanged = function (event) {
-  var newUrlId = event.newURL ? desktop.getElement(event.newURL) : null;
-  var oldUrlId = event.oldURL ? desktop.getElement(event.oldURL) : null;
-
-  if (newUrlId !== undefined) {
-    newUrlId.style.zIndex = "99";
-  }
-  if (oldUrlId !== undefined) {
-    oldUrlId.style.zIndex = "1";
-  }
-  console.log(desktop.activeWindows);
-}; */
-
 //then, remove clicked class
-desktop.closeWindow = function () {
-  var ele = desktop.getElement(this.href);
-  ele.classList.remove("clicked");
-  ele.style.zIndex = "";
-  var index = desktop.activeWindows.indexOf(ele);
-  desktop.activeWindows.splice(index, 1);
-  console.log(desktop.activeWindows)
-  //rewrite another function
-  //https://developer.mozilla.org/en-US/docs/Web/API/History_API
-  window.history.back();
+desktop.closeWindow = function() {
+  var clickedWindow = desktop.getDomElementFromUrl(this.href);
+  clickedWindow.classList.remove("clicked");
+
+  function removeWindow(ele, arr) {
+    ele.style.zIndex = "";
+    var index = arr.indexOf(ele);
+    arr.splice(index, 1); 
+  }
+  removeWindow(clickedWindow, desktop.activeWindows); 
 };
 
-//hide element when close button is clicked
-//remove clicked class from element  when this button is clicked
+
 desktop.eventListeners = (function () {
   desktop.files.forEach((file) =>
-    file.addEventListener("click", desktop.handleClickIcon)
+    file.addEventListener("click", desktop.openWindow)
   );
-  window.addEventListener("hashchange", desktop.locationHashChanged);
   desktop.buttons.closeButton.forEach((button) =>
     button.addEventListener("click", desktop.closeWindow)
   );
 })();
+
+
+//TEST
+//Write an algorithm to play drum only when the pane is open and on top stack
+/*window.addEventListener('hashchange', function() {
+  changeLocation(location);
+  //location.hash = desktop.activeWindows[desktop.activeWindows.length-1].id;
+})*/
+
