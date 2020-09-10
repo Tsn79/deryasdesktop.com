7e4f5cd85102b0c3d89a49015845c65a667e5fb2
Derya Antonelli <derya.antonelli@gmail.com>
2020-09-10 22:09:21 +0100

    Add array to save active  windows and assign z-index in ascending order. Add hash function to change url location corresponding to the link clicked.

--- js/app.js
+++ js/app.js
@@ -9,38 +9,36 @@ document.querySelector("body").addEventListener("click", function () {
 var desktop = {
   buttons: {},
   files: {},
-  activeWindows: []
+  activeWindows: [],
 };
 
 desktop.buttons.closeButton = document.querySelectorAll(".close-btn");
 desktop.files = document.querySelectorAll(".file");
 
-
 desktop.openWindow = function (event) {
   var window = desktop.getDomElementFromUrl(event.currentTarget.href);
   window.classList.add("clicked");
- 
-  var navigate = function(ele, arr) { 
+
+  var navigate = function (ele, arr) {
     //find if open window is clicked before
-    if(arr.indexOf(ele) === -1) {
+    if (arr.indexOf(ele) === -1) {
       arr.push(ele);
-      for(let i=0; i<arr.length; i++) {
-        arr[i].style.zIndex = `${i+1}`;
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].style.zIndex = `${i + 1}`;
       }
     } else {
       //if window is already clicked, bring it on the top
       var index = arr.indexOf(ele);
       arr.splice(index, 1);
       arr.push(ele);
-      for(let i=0; i<arr.length; i++) {
-        arr[i].style.zIndex = `${i+1}`;
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].style.zIndex = `${i + 1}`;
       }
-    } 
-  }
-  
+    }
+  };
+
   navigate(window, desktop.activeWindows);
 };
-
 
 //return corresponding element from url location
 desktop.getDomElementFromUrl = function (urlLink) {
@@ -56,18 +54,17 @@ desktop.getDomElementFromUrl = function (urlLink) {
 };
 
 //then, remove clicked class
-desktop.closeWindow = function() {
+desktop.closeWindow = function () {
   var clickedWindow = desktop.getDomElementFromUrl(this.href);
   clickedWindow.classList.remove("clicked");
 
   function removeWindow(ele, arr) {
     ele.style.zIndex = "";
     var index = arr.indexOf(ele);
-    arr.splice(index, 1); 
+    arr.splice(index, 1);
   }
-  removeWindow(clickedWindow, desktop.activeWindows); 
+  removeWindow(clickedWindow, desktop.activeWindows);
 };
-
 
 desktop.eventListeners = (function () {
   desktop.files.forEach((file) =>
@@ -78,13 +75,13 @@ desktop.eventListeners = (function () {
   );
 })();
 
-
 //Reset hash according to window id
-window.onpopstate = function() {
-  var changeHash = function(activeWindows, location) {
-    var lastWindow = activeWindows? activeWindows[activeWindows.length-1] : null;
-    location.hash = lastWindow? lastWindow.id : "";
-  }
+window.onpopstate = function () {
+  var changeHash = function (activeWindows, location) {
+    var lastWindow = activeWindows
+      ? activeWindows[activeWindows.length - 1]
+      : null;
+    location.hash = lastWindow ? lastWindow.id : "";
+  };
   changeHash(desktop.activeWindows, location);
 };
-
