691a5aae6ac1080e2ae681becbb7f27accb803e8
Derya Antonelli <derya.antonelli@gmail.com>
2020-09-11 16:39:34 +0100

    Use web audio api to record, play, stop, pause drum sounds

--- js/drummer.js
+++ js/drummer.js
@@ -1,100 +1,122 @@
 const drummer = {
-    buttons: {}
+  buttons: {},
 };
 
 drummer.drummerPane = document.querySelector("#drummer");
+
 drummer.buttons.record = document.querySelector(".record");
 drummer.buttons.pause = document.querySelector(".pause");
 drummer.buttons.play = document.querySelector(".play");
 drummer.buttons.stop = document.querySelector(".stop");
+
 drummer.canvas = document.querySelector(".visualizer");
 drummer.ctx = drummer.canvas.getContext("2d");
 
-//test
-drummer.sounds = document.querySelector(".drum");
+drummer.sounds = document.querySelectorAll(".drum-sound");
 drummer.record = document.querySelector(".recorded-audio");
-
 drummer.recordingTimeMS = 5000;
 
-//audio recording
-//https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API
-//https://mdn.github.io/web-dictaphone/
-let audioCtx;
-let dest;
-let mediaRecorder;
-let clicked = false;
-let oscillatorNode;
-let gainNode;
-let constantNode;
+drummer.module = (function () {
+  var audioCtx,
+    dest,
+    mediaRecorder,
+    clicked = false,
+    chunks = [];
+  return {
+    audioCtx: audioCtx,
+    dest: dest,
+    mediaRecorder: mediaRecorder,
+    clicked: clicked,
+    chunks: chunks,
+  };
+})();
 
-let chunks = [];
-
-//BUGS
-//Sound does not play while recording
+//[X]Sound does not play while recording
+//[X]Include multiple sound files
+//[X]Give functionality cancel button
+//[X]When record is pressed, rewrite on existing audio node
+//[ ]Connect with canvas
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
 
+//***************************AUDIO RECORD***************************************//
+
 function init() {
-  audioCtx = new (window.AudioContext || window.webkitAudioContext || window.mozAudioContext)();
- 
-  source = audioCtx.createMediaElementSource(drummer.sounds);
+  //create an audio context
+  drummer.module.audioCtx = new (window.AudioContext ||
+    window.webkitAudioContext ||
+    window.mozAudioContext)();
 
-  dest = audioCtx.createMediaStreamDestination();
+  drummer.module.dest = drummer.module.audioCtx.createMediaStreamDestination();
 
-  mediaRecorder = new MediaRecorder(dest.stream);
+  drummer.module.mediaRecorder = new MediaRecorder(drummer.module.dest.stream);
 
-  source.connect(dest);
-  
-
-  mediaRecorder.ondataavailable = function(evt) {
-    // push each chunk (blobs) in an array
-    chunks.push(evt.data);
+  var createAudioTrack = function (audioCtx, audioEls) {
+    var tracks = [];
+    audioEls.forEach((ele) => {
+      var track = audioCtx.createMediaElementSource(ele);
+      tracks.push(track);
+    });
+    return {
+      connect: function (dest, ctxDest) {
+        tracks.forEach((track) => {
+          track.connect(dest);
+          track.connect(ctxDest);
+        });
+      },
+    };
   };
 
-  mediaRecorder.onstop = function(evt) {
+  createAudioTrack(drummer.module.audioCtx, Array.from(drummer.sounds)).connect(
+    drummer.module.dest,
+    drummer.module.audioCtx.destination
+  );
+
+  drummer.module.mediaRecorder.ondataavailable = function (evt) {
+    // push each chunk (blobs) in an array
+    drummer.module.chunks.push(evt.data);
+  };
+
+  drummer.module.mediaRecorder.onstop = function (evt) {
     // Make blob out of our blobs, and open it.
-    let blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+    let blob = new Blob(drummer.module.chunks, {
+      type: "audio/ogg; codecs=opus",
+    });
+    //create new audio here
     drummer.record.src = URL.createObjectURL(blob);
   };
 }
 
-drummer.buttons.record.addEventListener('click', function() {
-  if(!audioCtx) {
+drummer.buttons.record.addEventListener("click", function () {
+  if (!drummer.module.audioCtx) {
     init();
   }
 
-  if (!clicked) {
-    mediaRecorder.start();
+  if (!drummer.module.clicked) {
+    drummer.module.chunks = [];
+    drummer.module.mediaRecorder.start();
     drummer.buttons.record.style.background = "yellow";
-    console.log("start recording")
-    clicked = true;
+    console.log("start recording");
+    drummer.module.clicked = true;
   } else {
     drummer.buttons.record.style.background = "";
-    mediaRecorder.requestData();
-    mediaRecorder.stop();
+    drummer.module.mediaRecorder.requestData();
+    drummer.module.mediaRecorder.stop();
+    drummer.module.clicked = false;
   }
+});
 
-})
-
-drummer.buttons.play.addEventListener('click', function() {
+drummer.buttons.play.addEventListener("click", function () {
   drummer.record.play();
-})
+});
 
-
-drummer.buttons.pause.addEventListener('click', function() {
+drummer.buttons.pause.addEventListener("click", function () {
   drummer.record.pause();
-})
+});
 
-
-
-
-
-
-
-
-
-
-
+drummer.buttons.stop.addEventListener("click", function () {
+  drummer.record.load();
+});
 
 
 
@@ -108,7 +130,6 @@ drummer.drummerPane.addEventListener("keyup", function (e) {
   }
 });
 
-
 //focus drummer when it is open
 window.onhashchange = function () {
   if (location.hash === "#drummer") {
@@ -118,15 +139,14 @@ window.onhashchange = function () {
   }
 };
 
-
 drummer.drummerPane.addEventListener("keydown", function (e) {
-    var audioMatch = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    if (audioMatch) {
-      //add playing class to the corresponding data
-      document
-        .querySelector(`.key[data-key='${e.keyCode}']`)
-        .classList.add("playing");
-      audioMatch.autoplay = true;
-      audioMatch.load();
-    }
-  });
+  var audioMatch = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  if (audioMatch) {
+    //add playing class to the corresponding data
+    document
+      .querySelector(`.key[data-key='${e.keyCode}']`)
+      .classList.add("playing");
+    audioMatch.autoplay = true;
+    audioMatch.load();
+  }
+});
