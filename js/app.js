fbabf03df13eef975e3cc1a4e52d7caea6bfaf0e
Derya Antonelli <derya.antonelli@gmail.com>
2020-11-06 10:33:28 +0000

    Refactor variable names and functions for readability and optimization. Add function to change stacking order of the frames

--- js/app.js
+++ js/app.js
@@ -1,87 +1,112 @@
+import { className } from "../js_modules/helper_functions.mjs";
 //CLICK SOUND
-const clickSound = document.querySelector(".click-sound");
-document.querySelector("body").addEventListener("click", function () {
-  clickSound.autoplay = true;
-  clickSound.load();
-});
 
-//DESKTOP BUTTONS
+
+//https://interactjs.io/
+
 var desktop = {
   buttons: {},
   files: {},
-  activeWindows: [],
+  activeApps: [],
 };
 
 desktop.buttons.closeButton = document.querySelectorAll(".close-btn");
 desktop.files = document.querySelectorAll(".file");
+desktop.apps = document.querySelectorAll(".pane");
+desktop.click = document.querySelector(".click-sound");
 
-desktop.openWindow = function (event) {
-  var window = desktop.getDomElementFromUrl(event.currentTarget.href);
-  window.classList.add("clicked");
+desktop.openApp = function (event) {
+  if (
+    event.target.className !== "file-icon" ||
+    //exclude mail icon
+    event.target.parentNode.className === "file file__mail"
+  ) {
+    return;
+  }
 
-  var navigate = function (ele, arr) {
-    //find if open window is clicked before
-    if (arr.indexOf(ele) === -1) {
-      arr.push(ele);
-      for (let i = 0; i < arr.length; i++) {
-        arr[i].style.zIndex = `${i + 1}`;
-      }
-    } else {
-      //if window is already clicked, bring it on the top
-      var index = arr.indexOf(ele);
-      arr.splice(index, 1);
-      arr.push(ele);
-      for (let i = 0; i < arr.length; i++) {
-        arr[i].style.zIndex = `${i + 1}`;
-      }
+  var currApp =
+    desktop.getDomElementFromUrl(event.target.parentNode.href) ||
+    desktop.getDomElementFromUrl(event.target.href);
+  className.add(currApp, "clicked");
+  return desktop.setStackingOrder(currApp, desktop.activeApps);
+};
+
+desktop.setStackingOrder = function (currApp, activeApps) {
+  //find if file link is clicked before
+  if (activeApps.indexOf(currApp) === -1) {
+    activeApps.push(currApp);
+    for (var i = 0; i < activeApps.length; i++) {
+      activeApps[i].style.zIndex = "".concat(i + 1);
     }
-  };
+  } else {
+    //if file link is already clicked, bring corresponding pane on the top
+    var index = activeApps.indexOf(currApp);
+    activeApps.splice(index, 1);
+    activeApps.push(currApp);
 
-  navigate(window, desktop.activeWindows);
+    for (var _i = 0; _i < activeApps.length; _i++) {
+      activeApps[_i].style.zIndex = "".concat(_i + 1);
+    }
+  }
 };
 
 //return corresponding element from url location
 desktop.getDomElementFromUrl = function (urlLink) {
-  if (urlLink !== undefined && urlLink.includes("#")) {
-    var regex = /#/;
-    var hashIndexInLink = urlLink.search(regex);
-    var elementIdAfterHash = urlLink.slice(hashIndexInLink + 1);
+  if (urlLink !== undefined && urlLink.indexOf("#") !== -1) {
+    var regex = /#/,
+      hashIndexInLink = urlLink.search(regex),
+      elementIdAfterHash = urlLink.slice(hashIndexInLink + 1);
     if (elementIdAfterHash !== undefined) {
-      var ele = document.querySelector(`#${elementIdAfterHash}`);
+      var ele = document.querySelector("#".concat(elementIdAfterHash));
     }
   }
   return ele;
 };
 
-desktop.closeWindow = function () {
-  var clickedWindow = desktop.getDomElementFromUrl(this.href);
-  clickedWindow.classList.remove("clicked");
-
-  function removeWindow(ele, arr) {
-    ele.style.zIndex = "";
-    var index = arr.indexOf(ele);
-    arr.splice(index, 1);
-  }
-  removeWindow(clickedWindow, desktop.activeWindows);
+desktop.closeApp = function (event) {
+  event.stopPropagation();
+  var selected = desktop.getDomElementFromUrl(this.href);
+  className.remove(selected, "clicked");
+  return desktop.removeAppFromStack(selected, desktop.activeApps);
 };
 
-//Add event listeners to manage open/close windows
-desktop.eventListeners = (function () {
-  desktop.files.forEach((file) =>
-    file.addEventListener("click", desktop.openWindow)
-  );
-  desktop.buttons.closeButton.forEach((button) =>
-    button.addEventListener("click", desktop.closeWindow)
-  );
-})();
 
-//Reset hash according to window id
+desktop.removeAppFromStack = function(currApp, activeApps) {
+  currApp.style.zIndex = "";
+  var index = activeApps.indexOf(currApp);
+  if (index !== -1) {
+    activeApps.splice(index, 1);
+  }
+}
+
+desktop.changeStackingOrder = function (e) {
+  desktop.setStackingOrder(e.currentTarget, desktop.activeApps);
+  window.location.hash = e.currentTarget.id;
+};
+
+//EVENT LISTENERS
+
+desktop.buttons.closeButton.forEach((button) =>
+  button.addEventListener("click", desktop.closeApp)
+);
+
+//when clicked, stack the app pane on the top
+desktop.apps.forEach((app) => {
+  app.addEventListener("click", desktop.changeStackingOrder);
+});
+
+document.addEventListener("click", desktop.openApp);
+
+document.querySelector("body").addEventListener("click", function () {
+  desktop.click.autoplay = true;
+  desktop.click.load();
+});
+
+//Rename hash after active app
 window.onpopstate = function () {
-  var changeHash = function (activeWindows, location) {
-    var lastWindow = activeWindows
-      ? activeWindows[activeWindows.length - 1]
-      : null;
-    location.hash = lastWindow ? lastWindow.id : "";
+  var changeHash = function (apps, location) {
+    var latestApp = apps ? apps[apps.length - 1] : null;
+    location.hash = latestApp ? latestApp.id : "";
   };
-  changeHash(desktop.activeWindows, location);
+  return changeHash(desktop.activeApps, location);
 };
