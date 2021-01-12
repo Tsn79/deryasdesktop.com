b93326abe953af67c211d887b868fd4f2249e2c1
Derya Antonelli <derya.antonelli@gmail.com>
2021-01-12 15:24:33 +0000

    Set visual formatting

--- js/load.js
+++ js/load.js
@@ -1,23 +1,24 @@
 var load = {};
- load.showPage = function() {
-    document.querySelector("#body-container").style.display = "block";
-    document.querySelector("#loader").style.display = "none";
+load.showPage = function () {
+  document.querySelector("#body-container").style.display = "block";
+  document.querySelector("#loader").style.display = "none";
+};
+
+load.barWidth = 0;
+load.setProgressBar = function () {
+  if (load.barWidth === 100) {
+    load.barWidth = 0;
   }
 
-  load.barWidth = 0;
-  load.setProgressBar = function() { 
-      if(load.barWidth === 100) {
-          load.barWidth = 0;
-      }
+  load.barWidth += 10;
+  document.querySelector(".myBar").style.width = `${load.barWidth}%`;
+};
 
-      load.barWidth += 10;    
-    document.querySelector(".myBar").style.width = `${load.barWidth}%`; 
-      
-  }
-  
 load.progress = setInterval(load.setProgressBar, 800);
 
-  window.addEventListener("load", function(){
-    load.showPage();
-    clearInterval(load.progress);
-   })
+window.addEventListener("load", function () {
+  load.showPage();
+  clearInterval(load.progress);
+});
+
+
